afe4d23e04d75b9f537d6e81d63fcf4808a1e43b
Young <wuycug@hotmail.com>
2024-02-11 20:21:41 +0100

    0.0.22 2024-02-11

--- programs/ararpy/calc/arr.py
+++ programs/ararpy/calc/arr.py
@@ -20,6 +20,7 @@ import numpy as np
 from scipy.stats import distributions
 from types import MethodType
 from typing import Optional, List, Union
+from decimal import Decimal
 
 
 # =======================
@@ -564,6 +565,46 @@ def wtd_mean(a: list, e: list, sf: int = 1, adjust_error: bool = True):
         k1 = (1 / sum(wt)) ** .5
     k5 = distributions.chi2.sf(k4, df)
     return k0, k1, k2, k3, k4, k5
+
+
+# =======================
+# Number format
+# =======================
+def change_number_format(numbers: Union[list, tuple], flag: str = "Scientific", precision: int = None):
+    """
+    Parameters
+    ----------
+    precision
+    flag
+    numbers
+
+    Returns
+    -------
+
+    """
+    flag = flag.replace(" ", "")
+    _precision = [len(str(num).split(".")[1]) if "." in str(num) else 0 for num in numbers]
+    if flag.startswith("AddZero"):
+        precision = max(_precision) if precision is None else precision
+        for index, num in enumerate(numbers):
+            numbers[index] = str(num) + ('.' if '.' not in str(num) else '') + '0' * (precision - _precision[index])
+        return change_number_format(numbers, flag=flag.replace("AddZero", ""), precision=precision)
+
+    if flag.startswith("Scientific"):
+        numbers = [
+            ("{:." + f"{(len(str(num).replace('.', '')) - 1) if precision is None else precision}" + "e}").format(
+                Decimal(str(num))) for num in numbers]
+        return change_number_format(numbers, flag=flag.replace("Scientific", ""), precision=precision)
+
+    if flag.startswith("NonZero"):
+        numbers = [str(num).rstrip("0") if "." in str(num) else str(num) for num in numbers]
+        return change_number_format(numbers, flag=flag.replace("NonZero", ""), precision=precision)
+
+    if flag.startswith("Round"):
+        numbers = [str(round(float(num), min(_precision) if precision is None else precision)) for num in numbers]
+        return change_number_format(numbers, flag=flag.replace("Round", ""), precision=precision)
+
+    return numbers
 
 
 if __name__ == '__main__':
