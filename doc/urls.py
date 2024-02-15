a40fad7adb5fc4a85c2d37bfb7401b2a2df369fd
Young <wuycug@hotmail.com>
2024-02-15 23:35:03 +0100

    update documentation html

--- doc/urls.py
+++ doc/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('en', views.doc_en, name="doc_en"),
     path('zh-cn', views.doc_zh_cn, name="doc_zh_cn"),
     path('tutorial', views.tutorial, name="tutorial"),
+    path('tutorial/zh-cn', views.tutorial_zh_CN, name="tutorial"),
     path('deploy', views.deploy, name="deploy"),
+    path('deploy/zh-cn', views.deploy_zh_CN, name="deploy"),
     path('update_log', views.update_log, name="update_log"),
 ]
