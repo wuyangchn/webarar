8b5882b71556b530d16c5f3788c37e614dcfa053
Young <wuycug@hotmail.com>
2025-01-23 00:42:34 +0800

    250123

---
+++ programs/ararpy/argon_diffusion_simulator/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+# ==========================================
+# Copyright 2025 Yang 
+# webarar - __init__.py
+# ==========================================
+#
+#
+# 
+"""
+from . import main
